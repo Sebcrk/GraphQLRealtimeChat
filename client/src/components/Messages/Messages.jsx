4178f99277951246b1af137d6d7d58e0482ec882
Sebcrk <60935077+Sebcrk@users.noreply.github.com>
2022-11-11 07:53:59 -0500

    Scroll works, but scrollbar is not shown
    Hid the scrollbar and deleted unused code from Home component

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -2,58 +2,55 @@ import { useRef, useEffect } from "react";
 import { useGetRealtimeMessages } from "../../messages/custom-hooks";
 import styles from "./Messages.module.css";
 
-
 function Messages(props) {
   const { data, loading } = useGetRealtimeMessages();
 
-  const bottomRef = useRef()
+  const bottomRef = useRef();
 
   const scrollToBottom = () => {
-    bottomRef.current?.scrollIntoView({behavior: "auto"})
-  }
+    bottomRef.current?.scrollIntoView({ behavior: "auto" });
+  };
   useEffect(() => {
-    scrollToBottom()
-  }, [data])
+    scrollToBottom();
+  }, [data]);
 
   if (loading) return <h3>Loading messages...</h3>;
 
   return (
     <>
-      {data.messages.map((message) => {
-        const isCurrentUser = message.userId === props.userId;
-        return (
-          <div
-          className={styles.content}
-            key={message.id}
-            style={{
-              display: "flex",
-              justifyContent: isCurrentUser ? "flex-end" : "flex-start",
-              paddingBottom: "1em",
-            }}
-          >
-            {!isCurrentUser && (
-              <div className={styles.userAvatar}>
-                {message.username.slice(0, 1).toUpperCase()}
+        {data.messages.map((message) => {
+          const isCurrentUser = message.userId === props.userId;
+          return (
+            <div
+              key={message.id}
+              style={{
+                display: "flex",
+                justifyContent: isCurrentUser ? "flex-end" : "flex-start",
+              }}
+            >
+              {!isCurrentUser && (
+                <div className={styles.userAvatar}>
+                  {message.username.slice(0, 1).toUpperCase()}
+                </div>
+              )}
+              <div>
+                <h5 style={{ display: "flex", justifyContent: "flex-start" }}>
+                  {message.username}
+                </h5>
+                <p
+                  style={{
+                    background: isCurrentUser ? "#2C454F" : "#6FE1C0",
+                    color: isCurrentUser ? "#74EAD5" : "white",
+                  }}
+                  className={styles.message}
+                >
+                  {message.content}
+                </p>
               </div>
-            )}
-            <div>
-              <h5 style={{ display: "flex", justifyContent: "flex-start" }}>
-                {message.username}
-              </h5>
-              <p
-                style={{
-                  background: isCurrentUser ? "#2C454F" : "#6FE1C0",
-                  color: isCurrentUser ? "#74EAD5" : "white"
-                }}
-                className={styles.message}
-              >
-                {message.content}
-              </p>
             </div>
-          </div>
-        );
-      })}
-      <div ref={bottomRef}/>
+          );
+        })}
+      <div ref={bottomRef} />
     </>
   );
 }
